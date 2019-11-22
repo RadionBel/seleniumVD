5b3ceef40b8b90528ed5cce8ab7f41080f84d3a8
Radion <radion1412@gmail.com>
2019-11-22 13:41:47 +0200

    3

--- test_google_search.py
+++ test_google_search.py
@@ -5,19 +5,19 @@ import time
 import allure
 
 
-@allure.title('Результат поиска равна 12')
+@allure.title('Результат поиска равна 11')
 @allure.severity(Severity.BLOCKER)
-def test_google():
-    driver = WebDriver(executable_path='C://chromDriver//chromedriver.exe')
+def test_login():
+    driver = WebDriver(executable_path="C://chromDriver//chromedriver.exe")
     with allure.step('Открываем браузер'):
-        driver.get('https://panel-dev.t4u-ho.team/v2/#/auth/sign-in')
+        driver.get('https://panel-dev.t4u-ho.team/v3/affiliate-team-lead/#/a/login')
 
     with allure.step('Логин пользователя'):
-        search_input = driver.find_element_by_xpath('//*[@id="mat-input-0"]')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_username"]')
         search_input.send_keys('root')
-        search_input = driver.find_element_by_xpath('//*[@id="mat-input-1"]')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_pw"]')
         search_input.send_keys('OLS46J25ZQ0fns0Oq6')
-        search_input = driver.find_element_by_xpath('/html/body/ho-root/div/ho-sign-in-layout/div/div/div/ho-login/div/form/button/span')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="button_login"]')
         search_input.click()                                                                                                                 # login
         time.sleep(10)
 
@@ -27,7 +27,7 @@ def test_google():
 
     with allure.step('Поиск количества пунктов меню'):
         search_results = driver.find_elements_by_xpath('//li[@routerlinkactive="active"]')                                                    # проверка количества пунктов меню
-        assert len(search_results) == 12
+        assert len(search_results) == 11
         time.sleep(2)
         search_input = driver.find_element_by_xpath('/html/body/app/aff-admin-root/div/aff-sidebar/aff-sidebar-wrapper/div/div/div/nav/ul/li[3]/a/span')
         search_input.click()
@@ -35,21 +35,6 @@ def test_google():
 
     with allure.step('Вылогинюемся'):
         search_input = driver.find_element_by_xpath('/html/body/app/aff-admin-root/div/aff-sidebar/aff-sidebar-wrapper/div/div/div/aff-sidebar-user/div/div/div[3]/aff-logout-button/a')
-        search_input.click()
-
-    with allure.step('Логинимся с неправильным паролем'):
-        search_input = driver.find_element_by_xpath('//*[@id="mat-input-0"]')
-        search_input.send_keys('root')
-        search_input = driver.find_element_by_xpath('//*[@id="mat-input-1"]')
-        search_input.send_keys('OLS46J25ZQ0fwns0Oq6')
-        search_input = driver.find_element_by_xpath('/html/body/ho-root/div/ho-sign-in-layout/div/div/div/ho-login/div/form/button/span')
-        search_input.click()
-        search_input = driver.find_element_by_xpath('//*[@id="mat-input-1"]')
-        search_input.clear()
-        time.sleep(3)
-        search_input.send_keys('OLS46J25ZQ0fns0Oq6')
-        time.sleep(3)
-        search_input = driver.find_element_by_xpath('/html/body/ho-root/div/ho-sign-in-layout/div/div/div/ho-login/div/form/button/span')
         search_input.click()
 
     time.sleep(10)
--- test_login.py
+++ test_login.py
@@ -2,14 +2,16 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from allure_commons.types import Severity
 import time
 import allure
-@allure.title('Результат поиска равна 11')
+
+
 @allure.severity(Severity.BLOCKER)
 def test_login():
     driver = WebDriver(executable_path="C://chromDriver//chromedriver.exe")
     with allure.step('Открываем браузер'):
-        driver.get('https://panel-dev.t4u-ho.team/v3/affiliate-team-lead/#/a/login')
+        driver.get('https://marvel-panel.t4u-ho.team/v3/auth/#/a/login')
+        driver.maximize_window()
 
-    with allure.step('Логин пользователя'):
+    with allure.step('Логин admin old app'):
         search_input = driver.find_element_by_xpath('//*[@qa_id="input_username"]')
         search_input.send_keys('root')
         search_input = driver.find_element_by_xpath('//*[@qa_id="input_pw"]')
@@ -17,13 +19,17 @@ def test_login():
         search_input = driver.find_element_by_xpath('//*[@qa_id="button_login"]').click()                                                                                                                 # login
         time.sleep(10)
 
-    with allure.step('Проверяем правильность Title'):
-        assert driver.title == "Huge Offers"
-        time.sleep(2)                                                                                                                         # проверка Title
-    with allure.step('Поиск количества пунктов меню'):
-        search_results = driver.find_elements_by_xpath('//li[@routerlinkactive="active"]')                                                    # проверка количества пунктов меню
-        assert len(search_results) == 11
-        time.sleep(2)
+    with allure.step('Вылогинюемся'):
+        search_input = driver.find_element_by_xpath('/html/body/app/aff-admin-root/div/aff-sidebar/aff-sidebar-wrapper/div/div/div/aff-sidebar-user/div/div/div[3]/aff-logout-button/a').click()
+        time.sleep(10)
+
+    with allure.step('Логин admin new app'):
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_username"]')
+        search_input.send_keys('root')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_pw"]')
+        search_input.send_keys('OLS46J25ZQ0fns0Oq6')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="button_login"]').click()                                                                                                                 # login
+        time.sleep(15)
 
     with allure.step('Переход на новый фронт'):
         search_input = driver.find_element_by_xpath('/html/body/app/aff-admin-root/div/aff-header/aff-header-wrapper/div/div/div[2]/redirect-to-new-app/div/span').click()
@@ -34,8 +40,36 @@ def test_login():
         search_input.click()
         search_input = driver.find_element_by_xpath('//*[@qa_id="link_menu_log-out"]')
         driver.execute_script("arguments[0].click();", search_input)
+        time.sleep(10)
 
+    with allure.step('Логин manager old app'):
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_username"]')
+        search_input.send_keys('manager')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_pw"]')
+        search_input.send_keys('111111')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="button_login"]').click()                                                                                                                 # login
+        time.sleep(30)
 
-    time.sleep(10)
+    with allure.step('Вылогинюемся'):
+        search_input = driver.find_element_by_xpath('/html/body/app/aff-manager-root/div/aff-sidebar/aff-sidebar-wrapper/div/div/div/aff-sidebar-user/div/div/div[3]/aff-logout-button/a').click()
+        time.sleep(10)
 
+    with allure.step('Логин manager new app'):
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_username"]')
+        search_input.send_keys('manager')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="input_pw"]')
+        search_input.send_keys('111111')
+        search_input = driver.find_element_by_xpath('//*[@qa_id="button_login"]').click()                                                                                                                 # login
+        time.sleep(30)                                                                                                                     # проверка Title
+
+    with allure.step('Переход на новый фронт'):
+        search_input = driver.find_element_by_xpath('/html/body/app/aff-manager-root/div/aff-header/aff-header-wrapper/div/div/div[2]/redirect-to-new-app/div/span').click()
+        time.sleep(5)
+
+    with allure.step('Вылогинюемся'):
+        search_input = driver.find_element_by_xpath('//*[@qa_id="button_current-user"]')
+        search_input.click()
+        search_input = driver.find_element_by_xpath('//*[@qa_id="link_menu_log-out"]')
+        driver.execute_script("arguments[0].click();", search_input)
+        time.sleep(10)
     ...
